90cddb7a33af803c635521da29edfa7527151c7b
yonghapp <1904297283@qq.com>
2017-03-30 16:37:32 +0800

    sheying

---
+++ js/shoucang.js
@@ -0,0 +1,185 @@
+
+	function btn1(){
+	var a=document.getElementById('texts1');
+	var Icon=document.getElementById("icon1");
+		if (a.innerHTML=="546") {
+			a.innerHTML="547";
+			Icon.style.color="red";
+		}
+		else {
+			a.innerHTML="546";
+			Icon.style.color="#333";
+		}
+	};
+
+	function btn2(){
+	var a=document.getElementById('texts2');
+	var Icon=document.getElementById("icon2");
+		if (a.innerHTML=="132") {
+			a.innerHTML="133";
+		}
+		else {
+			a.innerHTML="132";
+			Icon.style.color="#333";
+		}
+	};
+
+	function btn3(){
+	var a=document.getElementById('texts3');
+	var Icon=document.getElementById("icon3");
+	var b=document.getElementById("box");
+		if (a.innerHTML==="339" || b.style.display==="none") {
+			a.innerHTML="340";
+			b.style.display="block";
+		}
+		else {
+			a.innerHTML="339";
+			Icon.style.color="#333";
+			b.style.display="none";
+		}
+	};	
+//------------------------------------------------第一名切换结束------------------------
+
+	function btn4(){
+	var a=document.getElementById('texts4');
+	var Icon=document.getElementById("icon4");
+		if (a.innerHTML=="546") {
+			a.innerHTML="547";
+			Icon.style.color="red";
+		}
+		else {
+			a.innerHTML="546";
+			Icon.style.color="#333";
+		}
+	};
+
+	function btn5(){
+	var a=document.getElementById('texts5');
+	var Icon=document.getElementById("icon5");
+		if (a.innerHTML=="132") {
+			a.innerHTML="133";
+		}
+		else {
+			a.innerHTML="132";
+			Icon.style.color="#333";
+		}
+	};
+
+	function btn6(){
+	var a=document.getElementById('texts6');
+	var Icon=document.getElementById("icon6");
+	var b=document.getElementById("box");
+		if (a.innerHTML==="339" || b.style.display==="none") {
+			a.innerHTML="340";
+			b.style.display="block";
+		}
+		else {
+			a.innerHTML="339";
+			Icon.style.color="#333";
+			b.style.display="none";
+		}
+	};	
+//------------------------------------------------第二名切换结束------------------------
+
+	function btn7(){
+	var a=document.getElementById('texts7');
+	var Icon=document.getElementById("icon7");
+		if (a.innerHTML=="511") {
+			a.innerHTML="512";
+			Icon.style.color="red";
+		}
+		else {
+			a.innerHTML="511";
+			Icon.style.color="#333";
+		}
+	};
+
+	function btn8(){
+	var a=document.getElementById('texts8');
+	var Icon=document.getElementById("icon8");
+		if (a.innerHTML=="123") {
+			a.innerHTML="124";
+		}
+		else {
+			a.innerHTML="123";
+			Icon.style.color="#333";
+		}
+	};
+
+	function btn9(){
+	var a=document.getElementById('texts9');
+	var Icon=document.getElementById("icon9");
+	var b=document.getElementById("box");
+		if (a.innerHTML==="289" || b.style.display==="none") {
+			a.innerHTML="290";
+			b.style.display="block";
+		}
+		else {
+			a.innerHTML="298";
+			Icon.style.color="#333";
+			b.style.display="none";
+		}
+	};	
+//------------------------------------------------第三名切换结束------------------------
+
+
+//------------------------------------------------取消收藏弹框--------------------------
+	function Obtn(){
+		var a=document.getElementById('bton');
+		// var html=document.documentElement;
+		if (a.style.display==='block') {
+			a.style.display='none';
+
+		}else{
+			a.style.display="block";
+		}
+	};
+
+	
+
+//------------------------------------------------第一名切换结束------------------------
+
+		function Obtn2(){
+		var a=document.getElementById('bton');
+		// var html=document.documentElement;
+		if (a.style.display==='block') {
+			a.style.display='none';
+
+		}else{
+			a.style.display="block";
+		}
+	};
+//------------------------------------------------第二名切换结束------------------------
+
+		function Obtn3(){
+		var a=document.getElementById('bton');
+		// var html=document.documentElement;
+		if (a.style.display==='block') {
+			a.style.display='none';
+
+		}else{
+			a.style.display="block";
+		}
+	};
+//------------------------------------------------第三名切换结束------------------------
+	function My(){
+		var a=document.getElementById("box");
+		var html=document.documentElement;
+		if (a.style.display==='block') {
+			a.style.display='none';
+
+		}else{
+			a.style.display="block";
+		}
+	};
+
+	var oBtn=document.getElementById('oBtn');
+		oBtn.onclick=function (){
+		var a=document.getElementById('bton');
+		if (a.style.display==='block') {
+			a.style.display='none';
+
+		}else{
+			a.style.display="block";
+		}
+		};
---
+++ js/tan.js
@@ -0,0 +1,59 @@
+	function Obtn(){
+		var a=document.getElementById('bton');
+		// var html=document.documentElement;
+		if (a.style.display==='none') {
+			a.style.display='block';
+
+		}else{
+			a.style.display="none";
+		}
+	};
+
+	
+
+//------------------------------------------------第一名切换结束------------------------
+
+		function Obtn2(){
+		var a=document.getElementById('bton');
+		// var html=document.documentElement;
+		if (a.style.display==="none") {
+			a.style.display="block";
+
+		}else{
+			a.style.display="none";
+		}
+	};
+//------------------------------------------------第二名切换结束------------------------
+
+		function Obtn3(){
+		var a=document.getElementById('bton');
+		// var html=document.documentElement;
+		if (a.style.display==='none') {
+			a.style.display='block';
+
+		}else{
+			a.style.display="none";
+		}
+	};
+//------------------------------------------------第三名切换结束------------------------
+	function My(){
+		var a=document.getElementById("box");
+		var html=document.documentElement;
+		if (a.style.display=="block") {
+			a.style.display="none";
+
+		}else{
+			a.style.display="none";
+		}
+	};
+
+	var oBtn=document.getElementById('oBtn');
+		oBtn.onclick=function (){
+		var a=document.getElementById('bton');
+				if (a.style.display==='block') {
+				a.style.display='none';
+
+			}else{
+				a.style.display="none";
+			}
+		};
